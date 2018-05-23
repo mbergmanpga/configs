ba18e35a55061df958017bcf8333d5668ba677bf
Michael Bergman <michael.bergman@vonage.com>
2018-05-23 07:25:33 -0700

    initial fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,19 @@
+source /home/mbergman2/.phpbrew/phpbrew.fishsource /home/mbergman2/.phpbrew/phpbrew.fish Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
+
+# Path to your custom folder (default path is ~/.oh-my-fish/custom)
+#set fish_custom $HOME/dotfiles/oh-my-fish
+
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
+
+# Custom plugins and themes may be added to ~/.oh-my-fish/custom
+# Plugins and themes can be found at https://github.com/oh-my-fish/
+Theme 'idan'
+Plugin 'theme'
+. /home/mbergman2/.phpbrew/phpbrew.fish
+alias lc="~/bin/colorls/colorls.rb"
+
+export PATH="$PATH:/opt/mssql-tools/bin"
+export ASPNETCORE_ENVIRONMENT="Development"
+export PATH="$PATH:/opt/mssql-tools/bin"
